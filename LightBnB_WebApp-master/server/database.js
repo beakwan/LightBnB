00506f46cf098d787bd3d7dce07e9028a6856acd
Beatrice Kwan <beatrice.c.kwan@gmail.com>
2021-07-22 20:56:12 +0000

    update add property function to use queries

--- LightBnB_WebApp-master/server/database.js
+++ LightBnB_WebApp-master/server/database.js
@@ -121,12 +121,12 @@ exports.getAllReservations = getAllReservations;
     query += `AND owner_id = $${values.length} `;
   }
   if (options.minimum_price_per_night) {
-    values.push(options.minimum_price_per_night);
-    query += `AND cost_per_night >= $${values.length} * 100 `;
+    values.push(options.minimum_price_per_night * 100);
+    query += `AND cost_per_night >= $${values.length} `;
   }
   if (options.maximum_price_per_night) {
-    values.push(options.maximum_price_per_night);
-    query += `AND cost_per_night <= $${values.length} * 100 `;
+    values.push(options.maximum_price_per_night * 100);
+    query += `AND cost_per_night <= $${values.length} `;
   }
 
   query += `
@@ -162,9 +162,14 @@ exports.getAllProperties = getAllProperties;
  * @return {Promise<{}>} A promise to the property.
  */
 const addProperty = function(property) {
-  const propertyId = Object.keys(properties).length + 1;
-  property.id = propertyId;
-  properties[propertyId] = property;
-  return Promise.resolve(property);
+  const values = [];
+  const query = `INSERT INTO properties (owner_id, title, description, thumbnail_photo_url, cover_photo_url, cost_per_night, street, city, province, post_code, country, parking_spaces, number_of_bathrooms, number_of_bedrooms) VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12, $13, $14) RETURNING *`;
+
+  values.push(Number(property.owner_id), property.title, property.description, property.thumbnail_photo_url, property.cover_photo_url, property.cost_per_night, property.street, property.city, property.province, property.post_code, property.country, Number(property.parking_spaces), Number(property.number_of_bathrooms), Number(property.number_of_bedrooms));
+
+  return pool
+    .query(query, values)
+    .then(res => res.rows)
+    .catch(err => console.log(err.message))
 }
 exports.addProperty = addProperty;
