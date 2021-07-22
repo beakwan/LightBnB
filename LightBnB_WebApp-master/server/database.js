7a26588cb958d6d535c94d29b05f161281c200fd
Beatrice Kwan <beatrice.c.kwan@gmail.com>
2021-07-22 18:27:20 +0000

    update get reservations function with query

--- LightBnB_WebApp-master/server/database.js
+++ LightBnB_WebApp-master/server/database.js
@@ -75,7 +75,21 @@ exports.addUser = addUser;
  * @return {Promise<[{}]>} A promise to the reservations.
  */
 const getAllReservations = function(guest_id, limit = 10) {
-  return getAllProperties(null, 2);
+  const query = `SELECT reservations.*, properties.*, avg(property_reviews.rating) as average_rating
+  FROM reservations
+  JOIN properties ON reservations.property_id = properties.id
+  JOIN property_reviews ON property_reviews.property_id = properties.id
+  WHERE reservations.guest_id = $1 
+  AND reservations.end_date < now()::DATE
+  GROUP BY properties.id, reservations.id
+  ORDER BY reservations.start_date
+  LIMIT $2`;
+  const values = [guest_id, limit];
+
+  return pool
+    .query(query, values)
+    .then(res => res.rows)
+    .catch(err => console.log(err.message))
 }
 exports.getAllReservations = getAllReservations;
 
